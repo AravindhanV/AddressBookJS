df92a824a54636750c70258240d955ba49c803bf
Aravindhan V <varavindhan2010@gmail.com>
2021-10-19 19:20:02 +0530

    ADDED: function to validate any given address

--- js/contact_form.js
+++ js/contact_form.js
@@ -2,3 +2,8 @@ const validateName = (name) => {
   let nameRegex = RegExp("^[A-Z][a-zA-Z]{2}[a-zA-Z\\s]*$");
   if (!nameRegex.test(name)) throw "Name is incorrect";
 };
+
+const validateAddress = (address) => {
+  let addressRegex = RegExp("$(.{3,}){2,}^");
+  if (!addressRegex.test(address)) throw "Address is incorrect";
+};
