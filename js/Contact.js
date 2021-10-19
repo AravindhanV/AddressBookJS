67698b02ba393bfa90ab3f3eb0ec460857dc7f5d
Aravindhan V <varavindhan2010@gmail.com>
2021-10-19 20:11:30 +0530

    ADDED: Contact class and it's dependency to contact_form.html

---
+++ js/Contact.js
@@ -0,0 +1,45 @@
+class Contact {
+  id;
+
+  get name() {
+    return this.name;
+  }
+  set name(name) {
+    this.name = name;
+  }
+
+  get address() {
+    return this.address;
+  }
+  set address(address) {
+    this.address = address;
+  }
+
+  get phone() {
+    return this.phone;
+  }
+  set phone(phone) {
+    this.phone = phone;
+  }
+
+  get city() {
+    return this.city;
+  }
+  set city(city) {
+    this.city = city;
+  }
+
+  get state() {
+    return this.state;
+  }
+  set state(state) {
+    this.state = state;
+  }
+
+  get pincode() {
+    return this.pincode;
+  }
+  set pincode(pincode) {
+    this.pincode = pincode;
+  }
+}
