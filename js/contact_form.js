d6b92ebaaf37f77214951484f7a4fdfba7c83fe4
Aravindhan V <varavindhan2010@gmail.com>
2021-10-19 19:24:13 +0530

    ADDED: function to validate a given phone number

--- js/contact_form.js
+++ js/contact_form.js
@@ -7,3 +7,8 @@ const validateAddress = (address) => {
   let addressRegex = RegExp("$(.{3,}){2,}^");
   if (!addressRegex.test(address)) throw "Address is incorrect";
 };
+
+const validatePhone = (phone) => {
+  let phoneRegex = RegExp("$(\\+\\d{2}|\\d{2})?\\d{10}^");
+  if (!phoneRegex.test(phone)) throw "Phone number is incorrect";
+};
