7d0fbc5b62eb8387b2298a85224afdba8264a50e
Aravindhan V <varavindhan2010@gmail.com>
2021-10-20 19:40:24 +0530

    ADDED: server base URL to site_properties

--- js/site_properties.js
+++ js/site_properties.js
@@ -2,4 +2,5 @@ let site_properties = {
   home_page: "../pages/home.html",
   add_contact_page: "../pages/contact_form.html",
   use_local_storage: false,
+  server_url: "http://127.0.0.1:3000/AddressBookDB/",
 };
